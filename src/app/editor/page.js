0f2616ad68995417c03a6c6bc4155e5bd3fc4f77
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-10-30 02:09:11 +0530

    Edited editor page for 50% screen bifurcation

--- src/app/editor/page.js
+++ src/app/editor/page.js
@@ -1,5 +1,7 @@
 import Editor from "@/components/Editor/Editor";
+import Link from "next/link";
 import React from "react";
+import { AiFillHome } from "react-icons/ai";
 
 const page = () => {
   const sections = {
@@ -13,13 +15,20 @@ const page = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="relative flex h-screen items-center justify-center">
+      <Link
+        href="/"
+        className="absolute right-6 top-6 grid aspect-square w-[50px] place-items-center rounded-full bg-Green"
+      >
+        <AiFillHome className="text-xl text-White" />
+      </Link>
+      {/* preview and draggable section  */}
+      <div className="h-full w-1/2 border border-red-500 bg-Green"></div>
+
       {/* editor sections */}
-      <div className="h-full w-1/2 border border-blue-500">
+      <div className="h-auto w-1/2 border border-blue-500 ">
         <Editor sections={sections} />
       </div>
-      {/* preview and draggable section  */}
-      <div className="h-full w-1/2 border border-red-500"></div>
     </div>
   );
 };
